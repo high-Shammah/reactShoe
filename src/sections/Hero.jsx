a0f304226ede44f28ecc2c96db7479012eece102
Shammah Ugbebor <116138132+high-Shammah@users.noreply.github.com>
2023-11-27 14:12:43 -0800

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,30 +10,26 @@ const Hero = () => {
   const [ bigShoeImg, setBigShoeImg] = 
   useState(bigShoe1)
   return (
-    <section id="home" className="w-full flex
-     xl:flex-row flex-col justify-center 
-   min-h-screen gap-10 max-container" >
-    <div className="relative xl:w-2/5 flex
-    flex-col justify-center items-start
-    w-full max-xl:padding-x pt-28">
-      <p className="text-xl
-      font-montserrat text-coral-red">
-        Our Summer Collection</p>
-      <h1 className="mt-10 font-palanquin
-      text-8xl max-sm:text-[72px]
-      max-sm:leading-[82] font-bold">
-        <span className="xl:bg-white
-        xl:whitespace-nowrap relative z-10
-        pr-10">The New Arrival</span>
-        <br />
-        <span className=" text-coral-red
-        inline-block mt-3">Nike</span> Shoes
-      </h1>
-      <p className="font-montserrat text-slate-gray text-lg
-      leading-8 mt-6 mb-14 sm:max-w-sm">
-        Discover stylish Nike arrivals, quality 
-        comfort, and innovation for your active life
-      </p>
+    <section
+      id='home'
+      className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
+    >
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
+        <p className='text-xl font-montserrat text-coral-red'>
+          Our Summer collections
+        </p>
+
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
+          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
+            The New Arrival
+          </span>
+          <br />
+          <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
+        </h1>
+        <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
       <Button label="Shop now"
       iconURL={arrowRight} />
 
@@ -53,7 +49,7 @@ const Hero = () => {
     </div>
 
     <div className="relative flex-1 flex justify-center 
-    items-center xl:min-h-screen max-xl:py-40 bg-primary
+    items-center xl:min-h-screen max-xl:py-4 bg-primary
     bg-hero bg-cover bg-center">
       <img src={bigShoeImg} 
       alt="Shoe collection"
